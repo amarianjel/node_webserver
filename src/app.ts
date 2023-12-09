b9f6e9dbd84e73b858fd54733ecc7f1f0c9eae67
amarianjel <abrm.enginner@gmail.com>
2023-12-09 20:05:33 -0300

    Webserver http/1

--- src/app.ts
+++ src/app.ts
@@ -1,1 +1,12 @@
-console.log('first time')
+import http from 'http';
+
+const server = http.createServer((req, res) => {
+    console.log(req.url);
+
+    res.write('Hola Mundo');
+    res.end();
+});
+
+server.listen(8080, () => {
+    console.log('Server running on port 8080');
+});
