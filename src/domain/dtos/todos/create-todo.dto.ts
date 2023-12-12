1a544d9d1feb57957d7db38654040b7b0ebb374a
amarianjel <abrm.enginner@gmail.com>
2023-12-12 20:53:31 -0300

    Fix - Fecha en crear

--- src/domain/dtos/todos/create-todo.dto.ts
+++ src/domain/dtos/todos/create-todo.dto.ts
@@ -2,16 +2,22 @@ export class CreateTodoDto {
 
   private constructor(
     public readonly text: string,
+    public readonly completedAt?: Date,
   ){}
 
 
   static create( props: {[key:string]: any} ): [string?, CreateTodoDto?]  {
 
-    const { text } = props;
+    const { text, completedAt } = props;
 
     if ( !text ) return ['Text property is required', undefined];
 
+    const parsedCompletedAt = completedAt ? new Date(completedAt) : new Date();
 
-    return [undefined, new CreateTodoDto(text)];
+    if (completedAt && isNaN(parsedCompletedAt.getTime())) {
+      return ["Invalid format for completedAt", undefined];
+    }
+
+    return [undefined, new CreateTodoDto(text, parsedCompletedAt)];
   }
 }
