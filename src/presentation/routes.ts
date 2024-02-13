8e9ec5ac66ea2152d07c3fe799863b920a3bf7d4
amarianjel <abrm.enginner@gmail.com>
2024-02-12 22:08:30 -0300

    Consumir casos de usos

--- src/presentation/routes.ts
+++ src/presentation/routes.ts
@@ -1,12 +1,10 @@
 import { Router } from "express";
-import { TodoController } from "./todos/controller";
 import { TodoRoutes } from "./todos/routes";
 
 export class AppRoutes{
     static get routes(): Router{
 
         const router = Router();
-        const todoController = new TodoController();
 
         router.use('/api/todos', TodoRoutes.routes );
 
